d4c5ec31e3c68f44b7912cb3d7c6cae1629b53e5
[zhuliubao] <zhuliubao@qie.tv>
2021-01-08 11:15:41 +0800

    fix: 简易的lru实现

--- lru/lru.go
+++ lru/lru.go
@@ -8,7 +8,7 @@ import "container/list"
 2、maxBytes 是允许使用的最大内存，nbytes 是当前已使用的内存；
 3、键值对 entry 是双向链表节点的数据类型，在链表中仍保存每个值对应的 key 的好处在于，淘汰队首节点时，需要用 key 从字典中删除对应的映射。
 4、为了通用性，我们允许值是实现了 Value 接口的任意类型，该接口只包含了一个方法 Len() int，用于返回值所占用的内存大小。
- */
+*/
 type Cache struct {
 	maxBytes int64
 	nbytes   int64
@@ -40,31 +40,30 @@ func New(maxBytes int64) *Cache {
 2、不存在则是新增场景，首先队尾添加新节点 &entry{key, value}, 并字典中添加 key 和节点的映射关系。
 更新 c.nbytes，如果超过了设定的最大值 c.maxBytes，则移除最少访问的节点。
 
- */
+*/
 func (c *Cache) Add(key string, value Value) {
-	if ele,ok :=c.cache[key];ok {
+	if ele, ok := c.cache[key]; ok {
 		c.ll.MoveToFront(ele)
 		kv := ele.Value.(*Entry)
 		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
 		kv.value = value
-	}else {
-		ele := c.ll.PushFront(&Entry{key:key,value:value})
+	} else {
+		ele := c.ll.PushFront(&Entry{key: key, value: value})
 		c.cache[key] = ele
 		c.nbytes += int64(len(key)) + int64(value.Len())
 	}
 
-	for c.maxBytes != 0 && c.maxBytes <c.nbytes {
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
 		// maxbytes 设置为0 ，代表不对内存大小设限，这里和groupcache 一致，所以不为0时，才会判断是否超过了限制
-		c.RemoveOldest();
+		c.RemoveOldest()
 	}
-
 
 }
 
 /**
 如果键对应的链表节点存在，则将对应节点移动到队尾，并返回查找到的值。
 c.ll.MoveToFront(ele)，即将链表中的节点 ele 移动到队尾（双向链表作为队列，队首队尾是相对的，在这里约定 front 为队尾）
- */
+*/
 func (c *Cache) Get(key string) (value Value, ok bool) {
 
 	if ele, ok := c.cache[key]; ok {
@@ -79,7 +78,7 @@ func (c *Cache) Get(key string) (value Value, ok bool) {
 	c.ll.Back() 取到队首节点，从链表中删除。
 	delete(c.cache, kv.key)，从字典中 c.cache 删除该节点的映射关系。
 	更新当前所用的内存 c.nbytes。
- */
+*/
 func (c *Cache) RemoveOldest() {
 	ele := c.ll.Back()
 	if ele != nil {
@@ -93,3 +92,9 @@ func (c *Cache) RemoveOldest() {
 func (c *Cache) Len() int {
 	return c.ll.Len()
 }
+
+/**
+说明： 该lru只是一个简易的实现  lru当然是比较权衡的方案，
+但是比如周期性或者突发的查询会造成缓存的命中了下降，或者说缓存污染吧，
+其实还应该介绍下lru-k的方案，lru可以认为是lru-1， 那么lru-k就可以结合lfu的一些优点了。
+*/
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -3,57 +3,67 @@ package lru
 import (
 	"fmt"
 	"testing"
-	)
+)
 
 type String string
 
-func (d String)Len()int  {
-	return  len(d)
+func (d String) Len() int {
+	return len(d)
 }
 
 func TestGet(t *testing.T) {
 
-	lru := New(60)
-	lru.Add("key1",String("123456"))
-	if v,ok :=lru.Get("key1"); !ok || string(v.(String)) != "123456" {
+	lru := New(20)
+	fmt.Println(len("123456"))
+	lru.Add("key1", String("123456"))
+	lru.Add("key2", String("123457"))
+	lru.Add("key3", String("123458"))
+	v, _ := lru.Get("key2")
+
+	fmt.Println(v)
+
+	fmt.Println(lru.Get("key3"))
+	fmt.Println(lru.Get("key1"))
+
+	println(lru.Len())
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "123456" {
 		t.Fatalf("cache hit key1= 1234 failed")
 	}
 
-	if _,ok := lru.Get("key2");ok {
+	if _, ok := lru.Get("key2"); ok {
 		t.Fatalf("cache miss key2 failed")
 	}
 
 }
 
-func TestAdd( t *testing.T)  {
+func TestAdd(t *testing.T) {
 	lru := New(int64(0))
-	lru.Add("key",String("1"))
-	lru.Add("key",String("2222"))
-	fmt.Printf("any %d",lru.nbytes)
-	if lru.nbytes != int64(len("key") + len("2222")){
-		t.Fatal("expect 6 but got",lru.nbytes)
+	lru.Add("key", String("1"))
+	lru.Add("key", String("2222"))
+	fmt.Printf("any %d", lru.nbytes)
+	if lru.nbytes != int64(len("key")+len("2222")) {
+		t.Fatal("expect 6 but got", lru.nbytes)
 	}
 }
 
-
 func TestRemoveOldest(t *testing.T) {
 
-	k1,k2,k3 := "key1","key2","k3"
+	k1, k2, k3 := "key1", "key2", "k3"
 
-	v1,v2,v3 := "value1","value2","value3"
+	v1, v2, v3 := "value1", "value2", "value3"
 
-	cap := len(k1+k2+v1+v2)
+	cap := len(k1 + k2 + v1 + v2)
 
 	println(cap)
 
 	lru := New(int64(cap))
 
-	lru.Add(k1,String(v1))
-	lru.Add(k2,String(v2))
-	lru.Add(k3,String(v3))
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
 
 	println(lru.Len())
-	if _,ok :=lru.Get("key1");ok || lru.Len() !=2 {
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
 		t.Fatalf("Removeoldtest key1 failed")
 	}
 }
